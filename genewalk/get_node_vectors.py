bf939f60c1aaaf0cc5fe9dc4f49a782496438698
Ben Gyori <ben.gyori@gmail.com>
2019-06-19 14:38:41 -0400

    Basic style changes in get node vectors

--- genewalk/get_node_vectors.py
+++ genewalk/get_node_vectors.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
-import networkx as nx
-import indra
-import pickle as pkl
+import os
 import copy
 import time
+import logging
 import argparse
+import pickle as pkl
+import networkx as nx
 from genewalk.nx_mg_assembler import Nx_MG_Assembler
 from genewalk.deepwalk import DeepWalk
+
+
+logger = logging.getLogger('genewalk.get_node_vectors')
 
 
 if __name__ == '__main__':
     # Handle command line arguments
     parser = argparse.ArgumentParser(
-        description='Choose a path where GeneWalk files are generated (default: ~/genewalk/ ).')
+        description=('Choose a path where GeneWalk files are generated '
+                     '(default: ~/genewalk/ ).'))
     parser.add_argument('--path', default='~/genewalk/')
     parser.add_argument('--stmts', default='data/JQ1_HGNCidForINDRA_stmts.pkl')
     parser.add_argument('--fplx', default='data/JQ1_HGNCidForINDRA_fplx.txt')
@@ -22,51 +27,52 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Open pickled statements
-    print('loading', args.stmts)
-    with open(args.path+args.stmts, 'rb') as f:
-        stmts=pkl.load(f)
+    logger.info('loading %s' % args.stmts)
+    with open(os.path.join(args.path, args.stmts), 'rb') as f:
+        stmts = pkl.load(f)
 
-    print('assembling network')
-    MG=Nx_MG_Assembler(stmts,args.path_GO)
-    del(stmts)
+    logger.info('assembling network')
+    MG = Nx_MG_Assembler(stmts, args.path_GO)
+    del stmts
 
-    print('adding genes nodes from INDRA stmts')
+    logger.info('adding genes nodes from INDRA stmts')
     MG.MG_from_INDRA()
     MG.add_FPLXannotations(args.fplx)
-    print('number of gene nodes',nx.number_of_nodes(MG.graph))
-    print('adding GO nodes')
+    logger.info('number of gene nodes: %s' % nx.number_of_nodes(MG.graph))
+    logger.info('adding GO nodes')
     MG.add_GOannotations()
     MG.add_GOontology()
-    print('total number of nodes in network',nx.number_of_nodes(MG.graph))
+    logger.info('total number of nodes in network: %s' %
+                nx.number_of_nodes(MG.graph))
 
-    #pickle the network
-    filename='GeneWalk_MG.pkl'
-    MGA=copy.deepcopy(MG.graph)
-    with open(args.path+filename, 'wb') as f:
-        pkl.dump(MGA,f,protocol=pkl.HIGHEST_PROTOCOL)
-    del(MG)
+    # pickle the network
+    filename = 'GeneWalk_MG.pkl'
+    MGA = copy.deepcopy(MG.graph)
+    with open(os.path.join(args.path, filename), 'wb') as f:
+        pkl.dump(MGA, f, protocol=pkl.HIGHEST_PROTOCOL)
+    del MG
 
-    for rep in range(1,args.Nreps+1):
-        print(rep,'/',args.Nreps)
-        DW=DeepWalk(graph=MGA)
+    for rep in range(1, args.Nreps+1):
+        logger.info('%s/%s' % (rep, args.Nreps))
+        DW = DeepWalk(graph=MGA)
 
-        print('generate random walks')
+        logger.info('generate random walks')
         start = time.time()
         DW.get_walks()
         end = time.time()
-        print('DW.get_walks done ',end - start)#in sec
+        logger.info('DW.get_walks done %.2f' % (end - start))  # in sec
 
-        print('generate node vectors')
+        logger.info('generate node vectors')
         start = time.time()
         DW.word2vec()
         end = time.time()
-        print('DW.word2vec done',end - start)#in sec
+        logger.info('DW.word2vec done %.2f' % (end - start))  # in sec
 
-        ### Pickle the node vectors (embeddings) and DW object
+        # Pickle the node vectors (embeddings) and DW object
         nv = copy.deepcopy(DW.model.wv)
-        filename='GeneWalk_DW_nv_'+str(rep)+'.pkl'
-        with open(args.path+filename, 'wb') as f:
-            pkl.dump(nv,f)
-        filename='GeneWalk_DW_'+str(rep)+'.pkl'
-        with open(args.path+filename, 'wb') as f:
-            pkl.dump(DW,f)
+        filename = 'GeneWalk_DW_nv_%d.pkl' % rep
+        with open(os.path.join(args.path, filename), 'wb') as f:
+            pkl.dump(nv, f)
+        filename = 'GeneWalk_DW_%d.pkl' % rep
+        with open(os.path.join(args.path, filename), 'wb') as f:
+            pkl.dump(DW, f)
